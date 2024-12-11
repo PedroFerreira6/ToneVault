cc003ad3e028c6a3c645789cd1ec7cb77b6745e8
PedroFerreira6 <148778721+PedroFerreira6@users.noreply.github.com>
2024-12-11 12:22:03 +0000

    Correções nas listagens e footer do homeview

--- app/views/homeView.php
+++ app/views/homeView.php
@@ -56,11 +56,11 @@ https://templatemo.com/tm-577-liberty-market
         <div class="col-lg-6 align-self-center">
           <div class="header-text">
             <h6>TONEVAULT</h6>
-            <h2>Ganha Toins com o ToneVault!</h2>
-            <p>Com o ToneVault, tens a oportunidade de publicar e vender os teus áudios, enquanto interages com o site para ganhares Toins. Acumula Toins e utiliza-os para adquirir áudios incríveis na plataforma.</p>
+            <h2>Earn Toins with ToneVault!</h2>
+            <p>With ToneVault, you have the opportunity to publish and sell your audio while interacting with the site to earn Toins. Accumulate Toins and use them to purchase amazing audio on the platform.</p>
             <div class="buttons">
               <div class="border-button">
-                <a href="procurar">Procurar Audios</a>
+                <a href="procurar">Browse Audio</a>
               </div>
             </div>
           </div>
@@ -87,12 +87,12 @@ https://templatemo.com/tm-577-liberty-market
         <div class="col-lg-8">
           <div class="section-heading">
             <div class="line-dec"></div>
-            <h2>Publica o teu Audio agora!!!</h2>
+            <h2>Publish Your Audio Now!!!</h2>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="main-button">
-            <a href="publicar">PUBLICAR AUDIO</a>
+            <a href="publicar">PUBLISH AUDIO</a>
           </div>
         </div>
         <div class="col-lg-4">
@@ -103,8 +103,8 @@ https://templatemo.com/tm-577-liberty-market
             <div class="icon">
               <img src="app/assets/images/icon-03.png" alt="">
             </div>
-            <h4>Publica</h4>
-            <p>Publica os teus audios</p>
+            <h4>Publish</h4>
+            <p>Publish your audio files.</p>
           </div>
         </div>
         <div class="col-lg-4">
@@ -115,8 +115,8 @@ https://templatemo.com/tm-577-liberty-market
             <div class="icon">
               <img src="app/assets/images/icon-04.png" alt="">
             </div>
-            <h4>Vende</h4>
-            <p>Adiciona um preço aos teus audios.</p>
+            <h4>Sell</h4>
+            <p>Set a price for your audio files.</p>
           </div>
         </div>
         <div class="col-lg-4">
@@ -124,8 +124,8 @@ https://templatemo.com/tm-577-liberty-market
             <div class="icon">
               <img src="app/assets/images/icon-05.png" alt="">
             </div>
-            <h4>Ganha</h4>
-            <p>Ganha Toins com as compras nos teus audios</p>
+            <h4>Earn</h4>
+            <p>Earn Toins from purchases of your audio files.</p>
           </div>
         </div>
       </div>
@@ -138,7 +138,7 @@ https://templatemo.com/tm-577-liberty-market
         <div class="col-lg-6">
           <div class="section-heading">
             <div class="line-dec"></div>
-            <h2><em>Items</em> no mercado.</h2>
+            <h2><em>Items</em> on the market.</h2>
           </div>
         </div>
         <div class="col-lg-12">
@@ -146,47 +146,22 @@ https://templatemo.com/tm-577-liberty-market
             <div class="col-lg-6 currently-market-item all msc">
               <div class="item">
                 <div class="left-image">
-                  <img src="app/assets/images/market-01.jpg" alt="" style="border-radius: 20px; min-width: 195px;">
+                  <img src="app/assets/images/MusicImage.png" alt="" style="border-radius: 20px; min-width: 195px;max-height:300px">
                 </div>
                 <div class="right-content">
-                  <h4>Music Art Super Item</h4>
+                  <h4>MusicItem</h4>
                   <span class="author">
-                    <img src="app/assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
-                    <h6>Liberty Artist<br><a href="#">@libertyart</a></h6>
+                    <h6>AuthorProfile<br><a href="perfil"></h6>
                   </span>
                   <div class="line-dec"></div>
                   <span class="bid">
-                    Current Bid<br><strong>2.03 ETH</strong><br><em>($8,240.50)</em>
+                  Listen for<br><strong>0 Toins</strong><br>
                   </span>
                   <span class="ends">
-                    Ends In<br><strong>4D 08H 15M 42S</strong><br><em>(July 24th, 2022)</em>
+                    <br><strong>purchase rights</strong><br>
                   </span>
                   <div class="text-button">
-                    <a href="details.html">View Item Details</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-6 currently-market-item all dig">
-              <div class="item">
-                <div class="left-image">
-                  <img src="app/assets/images/market-01.jpg" alt="" style="border-radius: 20px; min-width: 195px;">
-                </div>
-                <div class="right-content">
-                  <h4>Digital Crypto Artwork</h4>
-                  <span class="author">
-                    <img src="app/assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
-                    <h6>Liberty Artist<br><a href="#">@libertyart</a></h6>
-                  </span>
-                  <div class="line-dec"></div>
-                  <span class="bid">
-                    Current Bid<br><strong>2.03 ETH</strong><br><em>($7,200.50)</em>
-                  </span>
-                  <span class="ends">
-                    Ends In<br><strong>2D 06H 30M 25S</strong><br><em>(July 26th, 2022)</em>
-                  </span>
-                  <div class="text-button">
-                    <a href="details.html">View Item Details</a>
+                    <a href="details.html">View Details</a>
                   </div>
                 </div>
               </div>
@@ -201,9 +176,8 @@ https://templatemo.com/tm-577-liberty-market
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
-          <p>Copyright © 2022 <a href="#">Liberty</a> NFT Marketplace Co., Ltd. All rights reserved.
-          &nbsp;&nbsp;
-          Designed by <a title="HTML CSS Templates" rel="sponsored" href="https://templatemo.com" target="_blank">TemplateMo</a></p>
+          <p>Copyright © 2024 ToneVault. All rights reserved.
+          &nbsp;&nbsp;</p>
         </div>
       </div>
     </div>
