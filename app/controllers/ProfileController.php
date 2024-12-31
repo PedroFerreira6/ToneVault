b6bdcb8ce53bf0d7d67eca921aa422898e8a7586
Otaniel Junior <otaniel1412@gmail.com>
2024-12-31 14:33:45 +0000

    provile V3 update
    visual update e iniciação da conexão a base de dados

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -4,18 +4,18 @@ class ProfileController
 {
     public function index()
     {
-       // if (isset($_SESSION['user_id'])) {
+       if (!isset($_SESSION['user_id'])) {
+            session_start();
             require_once './app/views/profileView.php';
-       // } else {
-           // header("location:/");
-       // }
+            $userId = $_SESSION['user_id'];
+
+            $userModel = new UserModel();
+            $userData = $userModel->getUserById($userId);
+            
+       } else {
+           header("location:/");
+       }
     }
 
-    public function loadProfile()
-    {
-    //$userId = $_SESSION['user_id'];
 
-        
-       
-    }
 }
--- app/views/profileView.php
+++ app/views/profileView.php
@@ -1,135 +1,281 @@
-<?php
+    <?php
 
-?>
-
+    ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
+
+  <head>
+
     <meta charset="utf-8">
-    <!--  This file has been downloaded from bootdey.com @bootdey on twitter -->
-    <!--  All snippets are MIT license http://bootdey.com/license -->
-    <title>Update user profile - Bootdey.com</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="app/assets/css/profile.css">
-    <link rel="icon" href="app/assets/images/logo/logo.svg" type="image/x-icon">
+    <meta name="author" content="templatemo">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Bootstrap core CSS -->
-<link href="app/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+
+    <title>Liberty NFT - Author Detail Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="app/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
 
-<!-- Additional CSS Files -->
-<link rel="stylesheet" href="app/assets/css/fontawesome.css">
-<link rel="stylesheet" href="app/assets/css/templatemo-liberty-market.css">
-<link rel="stylesheet" href="app/assets/css/owl.css">
-<link rel="stylesheet" href="app/assets/css/animate.css">
-<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-    </style>
-</head>
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="app/assets/css/fontawesome.css">
+    <link rel="stylesheet" href="app/assets/css/templatemo-liberty-market.css">
+    <link rel="stylesheet" href="app/assets/css/owl.css">
+    <link rel="stylesheet" href="app/assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+<!--
+
+-->
+  </head>
+
 <body>
+
+ 
 <?php include_once 'headerView.php'; ?>
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-<div class="container">
-    <div class="view-account">
-        <section class="module">
-            <div class="module-inner">
-                <div class="side-bar">
-                    <div class="user-info">
-                        <img class="img-profile img-circle img-responsive center-block" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
-                        <ul class="meta list list-unstyled">
-                            <li class="name">Rebecca Sanders
-                                <label class="label label-info">UX Designer</label>
-                            </li>
-                            <li class="email"><a href="#"><span class="__cf_email__" data-cfemail="4715222522242426691407302225342e33226924282a">[email&#160;protected]</span></a></li>
-                            <li class="activity">Last logged in: Today at 2:18pm</li>
-                        </ul>
-                    </div>
-            		<nav class="side-menu">
-        				<ul class="nav">
-        					<li class="active"><a href="#"><span class="fa fa-user"></span> Profile</a></li>
-        					<li><a href="#"><span class="fa fa-cog"></span> Settings</a></li>
-        					<li><a href="#"><span class="fa fa-credit-card"></span> Billing</a></li>
-        					<li><a href="#"><span class="fa fa-envelope"></span> Messages</a></li>
-        					
-        					<li><a href="user-drive.html"><span class="fa fa-th"></span> Drive</a></li>
-        					<li><a href="#"><span class="fa fa-clock-o"></span> Reminders</a></li>
-        				</ul>
-        			</nav>
+
+  <div class="author-page">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="author">
+            <img src="app/assets/images/single-author.jpg" alt="" style="border-radius: 50%; max-width: 170px;">
+            <h4><?php $userId;?><br> <a href="#">@melanie32</a></h4>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="right-info">
+            <div class="row">
+              <div class="col-4">
+                <div class="info-item">
+                  <i class="fa fa-heart"></i>
+                  <h6>1238 <em>Likes</em></h6>
                 </div>
-                <div class="content-panel">
-                    <h2 class="title">Profile<span class="pro-label label label-warning">PRO</span></h2>
-                    <form class="form-horizontal">
-                        <fieldset class="fieldset">
-                            <h3 class="fieldset-title">Personal Info</h3>
-                            <div class="form-group avatar">
-                                <figure class="figure col-md-2 col-sm-3 col-xs-12">
-                                    <img class="img-rounded img-responsive" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
-                                </figure>
-                                <div class="form-inline col-md-10 col-sm-9 col-xs-12">
-                                    <input type="file" class="file-uploader pull-left">
-                                    <button type="submit" class="btn btn-sm btn-default-alt pull-left">Update Image</button>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-md-2 col-sm-3 col-xs-12 control-label">User Name</label>
-                                <div class="col-md-10 col-sm-9 col-xs-12">
-                                    <input type="text" class="form-control" value="Rebecca">
-                                </div>
-                            </div>
-        
-                            <div class="form-group">
-                                <label class="col-md-2 col-sm-3 col-xs-12 control-label">First Name</label>
-                                <div class="col-md-10 col-sm-9 col-xs-12">
-                                    <input type="text" class="form-control" value="Rebecca">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-md-2 col-sm-3 col-xs-12 control-label">Last Name</label>
-                                <div class="col-md-10 col-sm-9 col-xs-12">
-                                    <input type="text" class="form-control" value="Sanders">
-                                </div>
-                            </div>
-                        </fieldset>
-                        <fieldset class="fieldset">
-                            <h3 class="fieldset-title">Contact Info</h3>
-                            <div class="form-group">
-                                <label class="col-md-2  col-sm-3 col-xs-12 control-label">Email</label>
-                                <div class="col-md-10 col-sm-9 col-xs-12">
-                                    <input type="email" class="form-control" value="Rebecca@website.com">
-                                    <p class="help-block">This is the email </p>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-md-2  col-sm-3 col-xs-12 control-label">Twitter</label>
-                                <div class="col-md-10 col-sm-9 col-xs-12">
-                                    <input type="text" class="form-control" value="SpeedyBecky">
-                                    <p class="help-block">Your twitter username</p>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-md-2  col-sm-3 col-xs-12 control-label">Linkedin</label>
-                                <div class="col-md-10 col-sm-9 col-xs-12">
-                                    <input type="url" class="form-control" value="https://www.linkedin.com/in/lorem">
-                                    <p class="help-block">eg. https://www.linkedin.com/in/yourname</p>
-                                </div>
-                            </div>
-                        </fieldset>
-                        <hr>
-                        <div class="form-group">
-                            <div class="col-md-10 col-sm-9 col-xs-12 col-md-push-2 col-sm-push-3 col-xs-push-0">
-                                <input class="btn btn-primary" type="submit" value="Update Profile">
-                            </div>
-                        </div>
-                    </form>
+              </div>
+              <div class="col-4">
+                <div class="info-item">
+                  <i class="fa fa-hand"></i>
+                  <h6>1238 <em>Likes</em></h6>
                 </div>
+              </div>
+              <div class="col-4">
+                <div class="info-item">
+                  <i class="fa fa-dollar"></i>
+                  <h6>1238 <em>Likes</em></h6>
+                </div>
+              </div>
             </div>
-        </section>
+            <div class="row">
+              <div class="col-5">
+                <h5>559 Followers</h5>
+              </div>
+              <div class="col-7">
+                <div class="main-button">
+                  <a href="#">Follow @melanie32</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-12">
+          <div class="section-heading">
+            <div class="line-dec"></div>
+            <h2>Melanie Smith’s <em>Items</em>.</h2>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="item">
+            <div class="row">
+              <div class="col-lg-12">
+                <span class="author">
+                  <img src="app/assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
+                </span>
+                <img src="app/assets/images/discover-03.jpg" alt="" style="border-radius: 20px;">
+                <h4>Mutant Ape Bored</h4>
+              </div>
+              <div class="col-lg-12">
+                <div class="line-dec"></div>
+                <div class="row">
+                  <div class="col-6">
+                    <span>Current Bid: <br> <strong>2.03 ETH</strong></span>
+                  </div>
+                  <div class="col-6">
+                    <span>Ends In: <br> <strong>25th Nov</strong></span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-12">
+                <div class="main-button">
+                  <a href="details.html">View Details</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="item">
+            <div class="row">
+              <div class="col-lg-12">
+                <span class="author">
+                  <img src="app/assets/images/author-02.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
+                </span>
+                <img src="app/assets/images/discover-06.jpg" alt="" style="border-radius: 20px;">
+                <h4>Mutant Ape Bored</h4>
+              </div>
+              <div class="col-lg-12">
+                <div class="line-dec"></div>
+                <div class="row">
+                  <div class="col-6">
+                    <span>Current Bid: <br> <strong>2.03 ETH</strong></span>
+                  </div>
+                  <div class="col-6">
+                    <span>Ends In: <br> <strong>25th Nov</strong></span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-12">
+                <div class="main-button">
+                  <a href="details.html">View Details</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="item">
+            <div class="row">
+              <div class="col-lg-12">
+                <span class="author">
+                  <img src="app/assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
+                </span>
+                <img src="app/assets/images/discover-05.jpg" alt="" style="border-radius: 20px;">
+                <h4>Mutant Ape Bored</h4>
+              </div>
+              <div class="col-lg-12">
+                <div class="line-dec"></div>
+                <div class="row">
+                  <div class="col-6">
+                    <span>Current Bid: <br> <strong>2.03 ETH</strong></span>
+                  </div>
+                  <div class="col-6">
+                    <span>Ends In: <br> <strong>25th Nov</strong></span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-12">
+                <div class="main-button">
+                  <a href="details.html">View Details</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="item">
+            <div class="row">
+              <div class="col-lg-12">
+                <span class="author">
+                  <img src="app/assets/images/author-03.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
+                </span>
+                <img src="app/assets/images/discover-04.jpg" alt="" style="border-radius: 20px;">
+                <h4>Mutant Ape Bored</h4>
+              </div>
+              <div class="col-lg-12">
+                <div class="line-dec"></div>
+                <div class="row">
+                  <div class="col-6">
+                    <span>Current Bid: <br> <strong>2.03 ETH</strong></span>
+                  </div>
+                  <div class="col-6">
+                    <span>Ends In: <br> <strong>25th Nov</strong></span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-12">
+                <div class="main-button">
+                  <a href="details.html">View Details</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script type="text/javascript">
-	
-</script>
-</body>
+  </div>
+
+  <div class="create-nft">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="section-heading">
+            <div class="line-dec"></div>
+            <h2>Create Your NFT & Put It On The Market.</h2>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="main-button">
+            <a href="create.html">Create Your NFT Now</a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="item first-item">
+            <div class="number">
+              <h6>1</h6>
+            </div>
+            <div class="icon">
+              <img src="app/assets/images/icon-02.png" alt="">
+            </div>
+            <h4>Set Up Your Wallet</h4>
+            <p>Lorem ipsum dolor sit amet, consectetu dipiscingei elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="item second-item">
+            <div class="number">
+              <h6>2</h6>
+            </div>
+            <div class="icon">
+              <img src="app/assets/images/icon-04.png" alt="">
+            </div>
+            <h4>Add Your Digital NFT</h4>
+            <p>Lorem ipsum dolor sit amet, consectetu dipiscingei elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="item">
+            <div class="icon">
+              <img src="app/assets/images/icon-06.png" alt="">
+            </div>
+            <h4>Sell Your NFT &amp; Make Profit</h4>
+            <p>Lorem ipsum dolor sit amet, consectetu dipiscingei elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>Copyright © 2022 <a href="#">Liberty</a> NFT Marketplace Co., Ltd. All rights reserved.
+          &nbsp;&nbsp;
+          Designed by <a title="HTML CSS Templates" rel="sponsored" href="https://templatemo.com" target="_blank">TemplateMo</a></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="app/vendor/jquery/jquery.min.js"></script>
+  <script src="app/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="app/assets/js/isotope.min.js"></script>
+  <script src="app/assets/js/owl-carousel.js"></script>
+
+  <script src="app/assets/js/tabs.js"></script>
+  <script src="app/assets/js/popup.js"></script>
+  <script src="app/assets/js/custom.js"></script>
+  </body>
 </html>
